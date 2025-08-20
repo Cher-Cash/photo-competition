282dd9e96fcf20d5c84c178b6556d44b3068dec0
Cher-Cash <kovach.aleksey19@yandex.ru>
2025-08-20 21:46:40 +1000

    fix authorization, need to fix endpoints

--- app/views/user.py
+++ app/views/user.py
@@ -4,9 +4,14 @@ import sqlalchemy as sa
 
 from app.extansions import db
 from app.models import Users
+from app.views.forms import LoginForm
 
 user_bp = Blueprint("user", __name__)
 
+
+@user_bp.route('/index')
+def index():
+    return render_template('index.html')
 
 @user_bp.route("/registration", methods=["GET", "POST"])
 def registration():
@@ -87,23 +92,26 @@ def registration():
 @user_bp.route("/authorization", methods=["GET", "POST"])
 def authorization():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('user.index'))
 
     form = LoginForm()
     if form.validate_on_submit():
+        print(form.email.data, form.password.data)
         user = db.session.scalar(
             sa.select(Users).where(Users.email == form.email.data))
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('user_bp.registration'))
+            print('wrong hole')
+            return redirect(url_for('user.authorization'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        print('внутри')
+        return redirect(url_for('user.index'))
     return render_template('authorization.html', title='Sign In', form=form)
 
 @user_bp.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('user.authorization'))
 
 
 
